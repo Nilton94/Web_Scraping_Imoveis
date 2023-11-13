5d3e97e8236366cf9207e9d1e90909262b0b6fa4
Nilton94 <josenilton1878@gmail.com>
2023-11-13 20:47:53 -0300

    Ajustes no App do Streamlit

--- app/scraping_imoveis_streamlit.py
+++ app/scraping_imoveis_streamlit.py
@@ -10,6 +10,7 @@ from utils.utils_psql import UtilsPSQL
 import datetime
 import pytz
 import plotly.graph_objects as go
+from utils.utils_views import StViews
 
 # ------------------------------------------ Dados dos municípios ----------------------------------------------#
 
@@ -18,9 +19,17 @@ df_mun = pd.read_parquet(
 )
 df_mun.sort_values(by = ['str_uf', 'str_local'], ascending = [True, True], inplace = True)
 locais = list(map(lambda x: x ,df_mun['str_local']))
-# locais.insert(0,'-- Selecione o Local --')
 
-st.markdown('#### DADOS DE ALUGUEL POR LOCAL E TIPO DE IMÓVEL - ZAPIMÓVEIS')
+# ---------------------------------------------- Page Config ----------------------------------------------------#
+
+st.set_page_config(
+    page_title = 'Dados de Aluguel - Zapimoveis',
+    page_icon = 'house',
+    layout = "wide"
+)
+
+st.header('_:blue[DADOS DE ALUGUEL POR LOCAL E TIPO DE IMÓVEL - ZAPIMÓVEIS]_')
+st.markdown("""----""")
 
 # ---------------------------------------------- Widgets --------------------------------------------------------#
 
@@ -67,165 +76,82 @@ ranking = st.sidebar.number_input(
     key = 'ranking'
 )
 
-# ------------------------------------------- Obtenção da base -------------------------------------------------------#
-# @st.cache
-def base():
+# ---------------------------------------------- Gerando os dados --------------------------------------------------------#
     
+if st.sidebar.button("Gerar Gráficos!", key = 'gerador'):
     try:
-        with st.empty():
-            st.write(f"{datetime.datetime.now(tz = None).replace(microsecond=0)} - :red[*Fazendo scraping dos dados...*]")
-            
-            ScraperZap().scraping_multiple(
-                _transacao = ['aluguel'], 
-                _tipo = st.session_state.tipo, 
-                _local = [st.session_state.local]
+        st.sidebar.write('Logs')
+        
+        st.sidebar.write(f"{datetime.datetime.now(tz = None).replace(microsecond=0)} - :red[*Obtendo a base...*]")
+        # Obtenção da base
+        df = StViews(local = st.session_state.local, tipo = st.session_state.tipo, ranking = st.session_state.ranking).check_base()
+        df_grouped = StViews(local = st.session_state.local, tipo = st.session_state.tipo, ranking = st.session_state.ranking).base_agg()
+
+        st.sidebar.write(f"{datetime.datetime.now(tz = None).replace(microsecond=0)} - :red[*Gerando cards...*]")
+        
+        # Cards
+        StViews(local = st.session_state.local, tipo = st.session_state.tipo, ranking = st.session_state.ranking).st_cards()
+
+        st.sidebar.write(f"{datetime.datetime.now(tz = None).replace(microsecond=0)} - :red[*Gerando plots...*]")
+        # Gráficos
+        df_plot = px.bar(
+            data_frame = df_grouped[df_grouped['rank'] <= int(st.session_state.ranking)],
+            x = 'bairro_f',
+            y = 'imoveis',
+            color = 'tipo',
+            # facet_col='local',
+            # facet_col_spacing = 0.2,
+            # facet_col_wrap= 1,
+            title = f'<b>Total de Imóveis por Tipo</b><br>Data: {df["data"].max()}',
+            labels = {'imoveis':'Imóveis','bairro_f':'Bairro'},
+            # text_auto = True
+            orientation='v',
+            width = 1100,
+            height = 500
+        )
+
+        # Definindo eixos y e x independentes
+        df_plot.update_yaxes(matches = None)
+        df_plot.update_xaxes(matches = None)
+
+        df_plot.for_each_yaxis(lambda yaxis: yaxis.update(showticklabels=True))
+
+        # Combo chart
+        df = df_grouped[df_grouped['rank'] <= int(st.session_state.ranking)].groupby('bairro_f').agg({'imoveis':'sum', 'aluguel':'mean'}).round(1).sort_values('imoveis', ascending = False).reset_index()
+        combo = go.Figure()
+        combo.add_trace(
+            go.Bar(
+                x = df['bairro_f'], 
+                y = df['imoveis'], 
+                name = 'Imóveis'
             )
+        )
 
-            # Base conforme parâmetros passados
-            df = pd.read_parquet(
-                os.path.join(os.getcwd(),'data','bronze','dados_imoveis_raw.parquet'), 
-                filters = [
-                    ("ano", "=", int(datetime.datetime.now(tz = pytz.timezone('America/Sao_Paulo')).strftime("%Y"))), 
-                    ("mes", "=", int(datetime.datetime.now(tz = pytz.timezone('America/Sao_Paulo')).strftime("%m"))), 
-                    ("dia", "=", int(datetime.datetime.now(tz = pytz.timezone('America/Sao_Paulo')).strftime("%d")))
-                ]
+        # Add a line trace from the DataFrame
+        combo.add_trace(
+            go.Scatter(
+                x = df['bairro_f'], 
+                y = df['aluguel'], 
+                mode = 'lines', 
+                name = 'Aluguel', 
+                yaxis = 'y2'
             )
+        )
 
-            st.write(f"{datetime.datetime.now(tz = None).replace(microsecond=0)} - :red[*Fazendo tratamento dos dados...*]")
-            # Tratamentos
-            # Remoção de outliers de aluguel
-            q1 = df.loc[
-                    (
-                        (df.local.isin([st.session_state.local])) 
-                        & (df.transacao == 'aluguel')
-                        & (df.tipo.isin(st.session_state.tipo))
-                    ),
-                    'aluguel'
-                ].quantile(0.25)
-            q3 = df.loc[
-                    (
-                        (df.local.isin([st.session_state.local])) 
-                        & (df.transacao == 'aluguel')
-                        & (df.tipo.isin(st.session_state.tipo))
-                    ),
-                    'aluguel'
-                ].quantile(0.75)
+        # Update layout
+        combo.update_layout(
+            title = 'Total de Imóveis e Média de Aluguel por Bairro',
+            xaxis = dict(title = 'Bairro'),
+            yaxis = dict(title = 'Imóveis'),
+            yaxis2 = dict(title = 'Média do Aluguel', overlaying = 'y', side = 'right'),
+            width = 1100,
+            height = 500
+        )
 
-            lim_sup = q3 + (q3-q1)*1.5
-            lim_inf = q1 - (q3-q1)*1.5
-
-            # Dataframe
-            df_grouped = (
-                    df
-                    .loc[
-                        (df.local.isin([st.session_state.local]))
-                        & df.transacao.isin(['aluguel'])
-                        & (df.tipo.isin(st.session_state.tipo))
-                        & (df.aluguel >= lim_inf)
-                        & (df.aluguel <= lim_sup)
-                    ]
-                    .groupby(by = ['local','bairro','tipo'])
-                    .agg(
-                        {
-                            'aluguel':'mean', 
-                            'id':'count',
-                            'area':'mean',
-                            'quartos':'mean',
-                            'chuveiros':'mean',
-                            'garagens':'mean'
-                        }
-                    )
-                    .rename(columns={'id':'imoveis'})
-                    .round(1)
-                    .sort_values(by = ['imoveis', 'aluguel'], ascending = [False, False])
-                    .reset_index()
-            )
-
-            # Coluna de rank de imóveis por bairro de cada local
-            df_rank = (
-                df_grouped
-                .groupby(by = ['local', 'bairro'])
-                .agg({'imoveis':'sum'})
-                .assign(rank = lambda x: x.groupby('local')['imoveis'].rank(ascending = False, method = 'min'))
-                .sort_values(by = 'imoveis', ascending = False)
-                .reset_index()
-            )
-
-            # Dataframe final agrupado
-            df_grouped = (
-                pd
-                .merge(df_grouped, df_rank[['bairro', 'rank']], on = 'bairro', how = 'left')
-                .sort_values(by = ['rank','imoveis'], ascending = [True, False])
-            )
-            df_grouped['bairro_f'] = df_grouped['bairro'].apply(lambda x: re.match(r'^([^,]+)', x).group(1))
-
-            # Gráfico
-            st.write(f"{datetime.datetime.now(tz = None).replace(microsecond=0)} - :red[*Plotando os gráficos...*]")
-
-            df_plot = px.bar(
-                data_frame = df_grouped[df_grouped['rank'] <= int(st.session_state.ranking)],
-                x = 'bairro_f',
-                y = 'imoveis',
-                color = 'tipo',
-                # facet_col='local',
-                # facet_col_spacing = 0.2,
-                # facet_col_wrap= 1,
-                title = f'<b>Total de Imóveis por Tipo</b><br>Data: {df["data"].max()}',
-                labels = {'imoveis':'Imóveis','bairro_f':'Bairro'},
-                # text_auto = True
-                orientation='v',
-                width = 1600,
-                height = 500
-            )
-
-            # Definindo eixos y e x independentes
-            df_plot.update_yaxes(matches=None)
-            df_plot.update_xaxes(matches=None)
-
-            df_plot.for_each_yaxis(lambda yaxis: yaxis.update(showticklabels=True))
-
-            # Combo chart
-            df = df_grouped[df_grouped['rank'] <= int(st.session_state.ranking)].groupby('bairro_f').agg({'imoveis':'sum', 'aluguel':'mean'}).round(1).sort_values('imoveis', ascending = False).reset_index()
-            combo = go.Figure()
-            combo.add_trace(
-                go.Bar(
-                    x = df['bairro_f'], 
-                    y = df['imoveis'], 
-                    name = 'Imóveis'
-                )
-            )
-
-            # Add a line trace from the DataFrame
-            combo.add_trace(
-                go.Scatter(
-                    x = df['bairro_f'], 
-                    y = df['aluguel'], 
-                    mode = 'lines', 
-                    name = 'Aluguel', 
-                    yaxis = 'y2'
-                )
-            )
-
-            # Update layout
-            combo.update_layout(
-                title = 'Total de Imóveis e Média de Aluguel por Bairro',
-                xaxis = dict(title = 'Bairro'),
-                yaxis = dict(title = 'Imóveis'),
-                yaxis2 = dict(title = 'Média do Aluguel', overlaying = 'y', side = 'right')
-            )
-
-            combo.update_layout(legend = dict(x = 1, y=1.3))
-
-            st.write('')
+        combo.update_layout(legend = dict(x = 1, y=1.3))
 
         st.plotly_chart(df_plot)
         st.plotly_chart(combo)
 
-    except:
-        return st.write(f'Problemas ao carregar os dados para a cidade de {st.session_state.local}')
-    
-if st.sidebar.button("Gerar Gráficos!", key = 'gerador'):
-    try:
-        base()
     except Exception as e:
         st.error(f'Erro na execução: {e}')
